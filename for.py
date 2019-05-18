281b9da57eccf864fcdfa1e5c51f450f0f9229b5
cristian-cloudmex <cristian.zambrano@cloudmex.io>
2019-05-18 12:47:42 -0500

    Added for and while documents

---
+++ for.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#Ejemplo de bucle for 
+#Desarrollado por el equipo de CloudMex Analytics
+
+list = ["3","Hola","Mundo","México"]
+
+#este bucle devuelve los valores de la lista
+for element in list:
+    print(element)
+
+#este for devuelve los indices de la lista en un rango de 0 a la longitud de la lista
+for element in range(len(list)):
+    print(element)
+
---
+++ while.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#Ejemplo de bucle while 
+#Desarrollado por el equipo de CloudMex Analytics
+
+contador = 1
+
+while(contador<=10):
+    print(contador)
+    contador=contador+1
